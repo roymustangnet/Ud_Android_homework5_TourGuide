47ad850f9f1adf0d2d3644bf61c5d5e9366a1410
roy mustang <roy.mustang.net@gmail.com>
2018-04-23 16:42:39 +0800

    add four activities

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.tourguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView attarctionsTextView = (TextView) findViewById(R.id.attractions);
+        attarctionsTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        TextView foodTextView = (TextView) findViewById(R.id.food);
+        foodTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        TextView hotelTextView = (TextView) findViewById(R.id.hotel);
+        hotelTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        TextView souvenirTextView = (TextView) findViewById(R.id.souvenir);
+        souvenirTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
